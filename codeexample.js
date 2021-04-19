caa4d72bcfde76558e3c32662d45447e98d2bac1
Raylen <81475574+ausername-1@users.noreply.github.com>
2021-04-19 22:14:12 +0300

    Create codeexample.js

---
+++ codeexample.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch')
+
+const params = {url: 'url', slug: ''}
+const data = await fetch('/api/shorten', {
+ method: 'post',
+ body: JSON.stringify(params)
+}).then((res => res.json()))
+
+console.log(data)
